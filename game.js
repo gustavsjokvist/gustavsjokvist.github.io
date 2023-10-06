d0e2cd2645f7f744d38874afb048dca7d0691114
Gustav Sjökvist <58043764+gustavsjokvist@users.noreply.github.com>
2023-10-06 12:17:25 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,30 @@
-let score = 0;
+let resources = {
+    wood: 0,
+    stone: 0
+};
 
-document.getElementById('click-button').addEventListener('click', function() {
-    score++;
-    document.getElementById('score').textContent = 'Score: ' + score;
-});
+let isGathering = {
+    wood: false,
+    stone: false
+};
+
+function gatherResource(type) {
+    if (!isGathering[type]) {
+        isGathering[type] = true;
+        let progress = 0;
+        const progressBar = document.getElementById(`${type}Progress`);
+        
+        const gatheringInterval = setInterval(() => {
+            progress += 10; // Filling the progress bar by 10% each 100ms
+            progressBar.value = progress;
+            
+            if (progress >= 100) {
+                clearInterval(gatheringInterval); // Stop interval
+                resources[type]++; // Add resource
+                document.getElementById(type).innerText = resources[type]; // Update display
+                progressBar.value = 0; // Reset progress bar
+                isGathering[type] = false; // Enable gathering again
+            }
+        }, 100); // Repeat every 100ms
+    }
+}
